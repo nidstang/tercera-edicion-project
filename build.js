85b9a1108ce9e60f227d277bbc0a0970e738f6a2
Pablo Fernández Franco <pableudo@gmail.com>
2025-11-03 19:30:27 +0000

    wip

--- build.js
+++ build.js
@@ -9,7 +9,31 @@ register(StyleDictionary, {
 const loader = ThemesLoader(StyleDictionary);
 
 async function run() {
-  console.log('hello world');
+  // this a console.log
+  // const name = "Pablo";
+  // console.log(name);
+
+  const pablo = {
+    name: "Pablo",
+    lastname: "Fernandez",
+    age: 33,
+    isAmazing: true,
+    location: {
+      city: 23,
+      country: "EspañaTrue",
+    },
+
+    greet: () => console.log('Hola Pablo'),
+  }
+
+  pablo.greet();
+
+  // console.log(pablo);
+
+  // console.log(pablo.name);
+  // console.log(pablo.lastname);
 }
+
+// tipos de parentesis {  } (  ) [  ]
 
 run();
