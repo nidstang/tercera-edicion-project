7258b903587ef07075354c3b63f0defe16e7d30f
Pablo Fernández Franco <pableudo@gmail.com>
2025-11-06 19:57:48 +0000

    clase jueves 6 nov

--- app/root.tsx
+++ app/root.tsx
@@ -16,12 +16,11 @@ import appStylesHref from "./app.scss?url";
 // descomentar cuando se generen las variables de dimensiones
 // import desktopVariablesHref from './build/desktop/variables.css?url';
 // import mobileVariablesHref from './build/mobile/variables.css?url';
-// import globalVariablesHref from './build/global/variables.css?url';
+import globalVariablesHref from './build/global/variables.css?url';
 
 // borrar cuando se generen las variables de dimensiones
 const desktopVariablesHref = '';
 const mobileVariablesHref = '';
-const globalVariablesHref = '';
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,7 +32,7 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap",
-  },
+  }
 ];
 
 export async function clientAction({ request }: Route.ClientActionArgs) {
--- build.js
+++ build.js
@@ -11,9 +11,15 @@ const loader = ThemesLoader(StyleDictionary);
 async function run() {
   const themes = await loader.load("/tokens")
 
+  themes.print();
+
   const globalTheme = themes.getThemeByName("global")
+  const lightTheme = themes.getThemeByName("light");
 
   const globalConfig = {
+    expand: {
+      typesMap: true
+    },
     platforms: {
       web: {
         files: [
@@ -23,13 +29,64 @@ async function run() {
           }
         ],
         transforms: [
-          "name/kebab"
+          "name/kebab",
+          "ts/resolveMath",
+          "size/pxToRem",
+          "ts/typography/fontWeight",
+          "ts/size/lineheight"
         ]
-      }
+      },
     }
   }
 
+  /**
+   * El fichero se tiene que crear en
+  la ruta: app/build/android/dimens.xml
+  Para hacer pruebas: node build.js
+   */
+  const androidConfig = {
+    expand: {
+      typesMap: true
+    },
+    platforms: {
+      web: {
+        files: [
+          {
+            format: "android/dimens",
+            destination: "app/build/android/dimens.xml"
+          }
+        ],
+        transforms: [
+          "name/camel",
+          "ts/resolveMath",
+          "size/pxToRem",
+          "ts/typography/fontWeight",
+          "ts/size/lineheight"
+        ]
+      },
+    }
+  }
+
+  const lightConfig = {
+    platforms: {
+      web: {
+        files: [
+          {
+            format: "css/variables",
+            destination: "app/build/light/variables.css"
+          }
+        ],
+        transforms: [
+
+        ]
+      },
+    }
+  }
+
+
   globalTheme.addConfig(globalConfig).build()
+  globalTheme.addConfig(androidConfig).build()
+  lightTheme.addConfig(lightConfig).build()
 
 
   // globalTheme.print()
